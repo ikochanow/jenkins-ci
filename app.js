8933f6ed0a5e59790d939daf8ecbf87dc068a8dc
ikochanow <42766104+ikochanow@users.noreply.github.com>
2018-08-28 12:42:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-
-res.send('hello world');
-res.send('hello jenkins3');
+var express = require('express');
+ 
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
